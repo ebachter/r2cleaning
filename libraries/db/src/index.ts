9b995a4af31c15830b13317998f17bf9f4cc9b8d
Emir Bakhtarov <bakhtarov@remrob.com>
2023-12-25 17:58:14 +0100

    refactor: add db package

---
+++ libraries/db/src/index.ts
@@ -0,0 +1,14 @@
+// import log from '@remrob/log';
+// import {register} from '@remrob/shuttle';
+// import mysql from 'mysql2/promise';
+
+const {MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB_MAIN} = process.env;
+
+// log.info('Initializing connection pool...');
+
+/* let prisma: PrismaClient<
+  Prisma.PrismaClientOptions,
+    'query' | 'info' | 'warn' | 'error'
+  >; */
+
+export const test = {test: 123};
