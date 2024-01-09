6ab650fc06704b996cbfe59cabecdae2294126ff
Emir Bakhtarov <bakhtarov@remrob.com>
2024-01-09 22:02:11 +0100

    refactor: add additional buttons

--- services/nativeapp/screens/details.tsx
+++ services/nativeapp/screens/details.tsx
@@ -8,8 +8,51 @@ export default function DetailsScreen({navigation}) {
   // const {message} = useAppSelector((state) => state.message);
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'aliceblue',
+        paddingLeft: 10,
+        paddingRight: 10,
+      }}
+    >
       <Text>Details Screen</Text>
+
+      <Button
+        style={{
+          marginTop: 10,
+          marginBottom: 5,
+          width: '100%',
+        }}
+        appearance="outline"
+        onPress={() => navigation.navigate('Order')}
+      >
+        Сделать заявку
+      </Button>
+
+      <Button
+        style={{
+          marginTop: 5,
+          width: '100%',
+        }}
+        appearance="outline"
+      >
+        Активные заказы
+      </Button>
+
+      <Button
+        style={{
+          marginTop: 10,
+          marginBottom: 40,
+          width: '100%',
+        }}
+        appearance="outline"
+      >
+        История заявок
+      </Button>
+
       <Button onPress={() => navigation.navigate('Home')}>Go to Home...</Button>
 
       <Button
