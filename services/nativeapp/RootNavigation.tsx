8a5d43a889979458630f2782aa1dce79a1ee0c6d
Emir Bakhtarov <bakhtarov@remrob.com>
2024-10-07 22:34:18 +0200

    refactor: refresh state after screen change

--- services/nativeapp/RootNavigation.tsx
+++ services/nativeapp/RootNavigation.tsx
@@ -1,10 +1,17 @@
 import {createNavigationContainerRef} from '@react-navigation/native';
 import {RootStackParamList} from './types/typesNavigation';
+import {CommonActions} from '@react-navigation/native';
 
 export const navigationRef = createNavigationContainerRef<RootStackParamList>();
 
-export function navigate(name, params) {
+export function navigate(name: keyof RootStackParamList, params) {
   if (navigationRef.isReady()) {
-    navigationRef.navigate(name, params);
+    // navigationRef.navigate(name, params);
+    navigationRef.dispatch(
+      CommonActions.reset({
+        index: 0,
+        routes: [{name, params}],
+      }),
+    );
   }
 }
